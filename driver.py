44cac4ddb2a0e9a806d1a889625f5f1f033f6c67
Rayhan Illyas <rayhan205732@gmail.com>
2022-11-28 00:28:03 +0700

    adjust model

--- driver.py
+++ driver.py
@@ -8,6 +8,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 4)
+    loaded_model = pickle.load(open("./model/model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        name = request.form['name']
+        reading_score = request.form['reading']
+        listening_score = request.form['listening']
+        speaking_score = request.form['speaking']
+        writing_score = request.form['writing']
+
+        to_predict_list = list(map(float, [reading_score, listening_score, speaking_score, writing_score]))
+        result = ValuePredictor(to_predict_list)
+
+        if float(result) == 0:
+            prediction = 'Noob'
+        elif float(result) == 1:
+            prediction = 'Oke'
+        elif float(result) == 2:
+            prediction = 'Ngeri'
+
+        return render_template("result.html", prediction=prediction, name=name)
+        
+
 
 if __name__ == '__main__':
     app.run(debug=True)
